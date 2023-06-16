75d1748e324446cdc71524c1afee2c4e47fd2349
aavidan <amir.avidan@beyondminds.ai>
2023-06-16 12:41:27 +0300

    Divide SummaryPage and NonSummaryPage which will be decided by isSummary

--- src/screens/ElectricVehicle/ElectricVehicle.js
+++ src/screens/ElectricVehicle/ElectricVehicle.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import HeaderScreen from './components/HeaderScreen.js';
+import SummaryPage from './components/SummaryPage.js';
+import NonSummaryPage from './components/NonSummaryPage.js';
 
 const ElectricVehicle = () => {
+    const isSummary = true
   return (
     <View style={{ flex: 1 }}>
-      <HeaderScreen />
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Electric Vehicle Screen</Text>
-      </View>
+      <HeaderScreen isSummary={isSummary} />
+      {isSummary ? <SummaryPage /> : <NonSummaryPage />}
       {/* Add the rest of your content below the header */}
     </View>
   );
---
+++ src/screens/ElectricVehicle/components/NonSummaryPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const NonSummaryPage = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Electric Vehicle Screen</Text>
+      {/* Add your non-summary page content */}
+    </View>
+  );
+};
+
+export default NonSummaryPage;
